2676e055bcbe89083924c553420a2c773a25c982
Andrej Maya <andrej.maya@nttdata.com>
2018-09-04 17:28:59 +0200

    further examples

--- webapp.go
+++ webapp.go
@@ -3,14 +3,67 @@ package main
 import (
     "fmt"
     "log"
-    "net/http"
+	"html/template"
+	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, is your name %s?", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	
+    log.Printf("Running on port 8080")
+    
+    http.HandleFunc("/", rootHandler)
+
+    staticTmpl := template.Must(template.ParseFiles("layout.html"))    
+	http.HandleFunc("/static", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		staticTmpl.Execute(w, data)
+    })
+    
+	formsTmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			formsTmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		// do something with details
+		_ = details
+
+		formsTmpl.Execute(w, struct{ Success bool }{true})
+	})    
+
+	http.ListenAndServe(":8080", nil)
 }
